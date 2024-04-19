93642d59770bf5e7d8930aca2790620061a461b3
o3ojunseok <o3ojunseok@gmail.com>
2024-04-19 14:30:30 +0900

    Feat: Service, Repository

--- url/src/main/java/com/example/url/service/SimpleShortenUrlService.java
+++ url/src/main/java/com/example/url/service/SimpleShortenUrlService.java
@@ -2,10 +2,18 @@ package com.example.url.service;
 
 import com.example.url.dto.ShortenUrlCreateRequestDto;
 import com.example.url.dto.ShortenUrlCreateResponseDto;
+import com.example.url.repository.ShortenUrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleShortenUrlService {
+    private final ShortenUrlRepository shortenUrlRepository;
+
+    @Autowired
+    SimpleShortenUrlService(ShortenUrlRepository shortenUrlRepository) {
+        this.shortenUrlRepository = shortenUrlRepository;
+    }
     public ShortenUrlCreateResponseDto generateShortenUrl(
             ShortenUrlCreateRequestDto shortenUrlCreateRequestDto
     ) {
